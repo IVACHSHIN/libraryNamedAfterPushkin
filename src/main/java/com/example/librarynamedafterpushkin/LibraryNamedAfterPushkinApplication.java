c42ccb5ca59b7178193a3aaa61ec5d8ef053ff69
Alexandr <alexandr.ivachshin@gmail.com>
2021-04-30 13:33:22 +0600

    init project

---
+++ src/main/java/com/example/librarynamedafterpushkin/LibraryNamedAfterPushkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarynamedafterpushkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryNamedAfterPushkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryNamedAfterPushkinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/librarynamedafterpushkin/LibraryNamedAfterPushkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarynamedafterpushkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryNamedAfterPushkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
