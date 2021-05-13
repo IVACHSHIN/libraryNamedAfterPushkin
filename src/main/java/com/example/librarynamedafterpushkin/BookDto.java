eff5e96e304defab08ba342b9a9b46144a95e7a8
Alexandr <alexandr.ivachshin@gmail.com>
2021-05-13 20:45:29 +0600

    validation with tests

---
+++ src/main/java/com/example/librarynamedafterpushkin/BookDto.java
@@ -0,0 +1,25 @@
+package com.example.librarynamedafterpushkin;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
+public class BookDto {
+    @NotBlank(message = "is mandatory")
+    private String name;
+
+    @NotBlank(message = "is mandatory")
+    private String author;
+
+    private String description;
+    private String publisher;
+
+    @Pattern(regexp = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$")
+    private String isbn;
+
+    @Range(min = 1800, max = 3000)
+    private Integer year;
+}
